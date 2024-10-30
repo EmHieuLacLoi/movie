263fca452fe50d32d81aa8e32246d015cf4adbe4
EmHieuLacLoi <vohieu972003@gmail.com>
2024-10-30 23:19:07 +0700

    Watch movie, crawl data...

--- src/app/controllers/movieController.js
+++ src/app/controllers/movieController.js
@@ -1,14 +1,22 @@
 const listMovie = require("../models/listMovie.js");
-const { multipleMongooseToObject } = require('../../utils/mongoose.js');
+const { multipleMongooseToObject } = require("../../utils/mongoose.js");
+const axios = require("axios");
+const BASE_URL = "https://phimapi.com/phim";
 
 class HomeController {
   show(req, res, next) {
-    listMovie.find({})
-      .then((movie) => {
-        movie = multipleMongooseToObject(movie)
-        res.render('movie', { movie })
-      })
-      .catch(next);
+    let link = req.path
+    axios.get(`${BASE_URL}${link}`)
+    .then(response  => {
+      let data = response.data
+      let list = data.episodes[0].server_data
+      let link_embed = {}
+      for (let i = 0; i < list.length; i++) {
+        link_embed[i] = list[i].link_embed
+      }
+      res.render('movie', { link_embed })
+    })
+    .catch(next) 
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -21,6 +21,9 @@ app.engine(
   "hbs",
   handlebars.engine({
     extname: ".hbs",
+    helpers: {
+      sum: (a, b) => a + b
+    }
   })
 );
 app.set("view engine", "hbs");
@@ -28,39 +31,6 @@ app.set("views", path.join(__dirname, "resources", "views"));
 
 // route
 route(app)
-
-// const axios = require('axios');
-// const BASE_URL = 'https://phimapi.com/v1/api/tim-kiem';
-
-// app.get('/', async (req, res) => {
-//   try {
-//       const response = await axios.get(`${BASE_URL}`, {
-//         params: {
-//           keyword: 'doraemon',
-//           limit: '10'
-//         }
-//       });
-
-//       const movies = response.data;
-
-//       // Render thông tin phim ra trang
-//       res.send(`
-//         <h1>Phim Mới Cập Nhật</h1>
-//         <ul>
-//             ${movies.data.items.map(movie => `
-//                 <li>
-//                     <strong>${movie.name}</strong> (Ngày phát hành: ${movie.slug})
-//                     <p>${movie.origin_name}</p>
-//                 </li>
-//             `).join('')}
-//         </ul>
-//     `);
-//   } catch (error) {
-//       console.error('Error fetching movies:', error);
-//       res.status(500).send('Error fetching movies.');
-//   }
-// });
-
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
