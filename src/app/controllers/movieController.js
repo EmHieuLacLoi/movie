336aa15971cb100abdaf64d6d7701c4c410faf10
EmHieuLacLoi <vohieu972003@gmail.com>
2024-11-02 10:14:39 +0700

    create api folder, change movie css, add slugify package

--- src/app/controllers/movieController.js
+++ src/app/controllers/movieController.js
@@ -1,34 +1,35 @@
-const listMovie = require("../models/listMovie.js");
-const { multipleMongooseToObject } = require("../../utils/mongoose.js");
-const axios = require("axios");
-const BASE_URL = "https://phimapi.com/phim";
+const api = require("../../config/api/index.js");
 
 class HomeController {
   show(req, res, next) {
-    let link = req.path
-    axios.get(`${BASE_URL}${link}`)
-    .then(response  => {
-      let data = response.data
-      let info_movie = {}
-      info_movie["name"] = data.movie.name
-      info_movie["origin_name"] = data.movie.origin_name
-      info_movie["status"] = data.movie.episode_current
-      info_movie['poster_url'] = data.movie.poster_url
-      info_movie['time'] = data.movie.time
-      info_movie['episode_total'] = data.movie.episode_total
-      info_movie['year'] = data.movie.year
-      info_movie['quality'] = data.movie.quality
-      info_movie['lang'] = data.movie.lang
-      
-      info_movie['watch_movie'] = []
-      let link = data.episodes[0].server_data
-      for (let i = 0; i < link.length; i++) {
-        info_movie['watch_movie'].push(link[i].link_embed)
-      }
+    let link = req.path;
+    api
+      .getData(link)
+      .then((data) => {
+        let info_movie = {};
+        info_movie["name"] = data.movie.name;
+        info_movie["origin_name"] = data.movie.origin_name;
+        info_movie["status"] = data.movie.episode_current;
+        info_movie["poster_url"] = data.movie.poster_url;
+        info_movie["time"] = data.movie.time;
+        info_movie["episode_total"] = data.movie.episode_total;
+        info_movie["year"] = data.movie.year;
+        info_movie["quality"] = data.movie.quality;
+        info_movie["lang"] = data.movie.lang;
+        info_movie["country"] = data.movie.country[0].name;
 
-      res.render('movie', { info_movie })
-    })
-    .catch(next) 
+
+        info_movie["watch_movie"] = [];
+        link = data.episodes[0].server_data;
+        for (let i = 0; i < link.length; i++) {
+          info_movie["watch_movie"].push(link[i].link_embed);
+        }
+
+        res.render("movie", { info_movie });
+      })
+      .catch((error) => {
+        next(error); 
+      });
   }
 }
 
---
+++ src/config/api/index.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+const BASE_URL = "https://phimapi.com/phim";
+
+async function getData(path) {
+    try {
+        const res = await axios.get(`${BASE_URL}${path}`);
+        return res.data;
+    } catch (e) {
+        console.log("Error get data: ", e);
+        throw e; 
+    }
+}
+
+module.exports = { getData }
